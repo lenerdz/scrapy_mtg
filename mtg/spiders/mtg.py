67400a8fa4579bc0d6c439d28edad7b77b50f37d
Nerd <gerardoccf@gmail.com>
2018-11-02 19:12:23 -0300

    printing when finishing a set and saving absolute urls

--- mtg/spiders/mtg.py
+++ mtg/spiders/mtg.py
@@ -10,7 +10,7 @@ class MtgSpider(scrapy.Spider):
 
     def parse(self, response):
         url = 'https://www.mtggoldfish.com'
-        urls = response.css("li > a::attr(href)").extract()
+        # urls = response.css("li > a::attr(href)").extract()
         for mtgset in response.css("li"):
             if mtgset.css("img::attr(src)") and mtgset.css("a::text"):
                 link = url + mtgset.css("a::attr(href)")[0].extract()
@@ -24,12 +24,15 @@ class MtgSpider(scrapy.Spider):
                 yield scrapy.Request(sets["link"], self.parse_set)
                 
     def parse_set(self, response):
+        url = 'https://www.mtggoldfish.com'
         for card in response.css("tr"):
             if card.css("td.card > a::text"):
+                link = url + card.css("td.card > a::attr(href)")[0].extract()
                 cards = {
                     'set': card.css("td:nth-child(2)::text")[0].extract(),
                     'name': card.css("td.card > a::text")[0].extract(),
-                    'link': card.css("td.card > a::attr(href)")[0].extract(),
+                    'link': link,
                     'image': card.css("td.card > a::attr(data-full-image)")[0].extract()
                 }
                 yield cards
+        print("Finished "+cards["set"])
