9bde2735e7baaccb986e641e4e3d57c79b615e8c
Gerardo Castro <gerardoccf@gmail.com>
2018-11-07 09:34:56 -0300

    updated spiders to have its own configuration

--- mtg/spiders/cards.py
+++ mtg/spiders/cards.py
@@ -5,15 +5,26 @@ import scrapy
 class CardsSpider(scrapy.Spider):
     name = 'cards'
     allowed_domains = ['mtggoldfish.com']
-    start_urls = ['https://www.mtggoldfish.com/index/M10#paper']
+    start_urls = ['https://www.mtggoldfish.com/prices/select']
+
+    custom_settings = {
+        'MYSQL_TABLE': 'cards'
+    }
 
     def parse(self, response):
+        url = 'https://www.mtggoldfish.com'
+        for mtgset in response.css("li"):
+            if mtgset.css("img::attr(src)") and mtgset.css("a::text"):
+                link = url + mtgset.css("a::attr(href)")[0].extract()
+                yield scrapy.Request(link, self.parse_set)
+
+    def parse_set(self, response):
         url = 'https://www.mtggoldfish.com'
         for card in response.css("tr"):
             if card.css("td.card > a::text"):
                 link = url + card.css("td.card > a::attr(href)")[0].extract()
                 cards = {
-                    'set': card.css("td:nth-child(2)::text")[0].extract(),
+                    'setname': card.css("td:nth-child(2)::text")[0].extract(),
                     'name': card.css("td.card > a::text")[0].extract(),
                     'link': link,
                     'image': card.css("td.card > a::attr(data-full-image)")[0].extract()
--- mtg/spiders/sets.py
+++ mtg/spiders/sets.py
@@ -7,15 +7,18 @@ class SetsSpider(scrapy.Spider):
     allowed_domains = ['mtggoldfish.com']
     start_urls = ['https://www.mtggoldfish.com/prices/select']
 
+    custom_settings = {
+        'MYSQL_TABLE': 'sets'
+    }
+
     def parse(self, response):
         url = 'https://www.mtggoldfish.com'
-        # urls = response.css("li > a::attr(href)").extract()
         for mtgset in response.css("li"):
             if mtgset.css("img::attr(src)") and mtgset.css("a::text"):
                 link = url + mtgset.css("a::attr(href)")[0].extract()
                 sets = {
                     'code': mtgset.css("img::attr(alt)")[0].extract(),
-                    'name': mtgset.css("a::text")[1].re('\n(.*)\n')[0],#extract(),
+                    'name': mtgset.css("a::text")[1].re('\n(.*)\n')[0],
                     'icon': mtgset.css("img::attr(src)")[0].extract(),
                     'url': link
                 }
