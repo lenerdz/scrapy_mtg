b5f22084f1c59c4e046ebc49c13c2db5b9359eb8
Gerardo Castro <gerardoccf@gmail.com>
2018-11-07 14:28:42 -0300

    updated cards spider to read start_urls from json file

--- mtg/spiders/cards.py
+++ mtg/spiders/cards.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
 
 
 class CardsSpider(scrapy.Spider):
     name = 'cards'
     allowed_domains = ['mtggoldfish.com']
-    start_urls = ['https://www.mtggoldfish.com/prices/select']
+    # start_urls = ['https://www.mtggoldfish.com/prices/select']
+
+    with open('data/sets.json') as f:
+        data = json.load(f)
+
+    start_urls = []
+    for x in data:
+        start_urls.append(x['url'])
 
     custom_settings = {
         'MYSQL_TABLE': 'cards'
@@ -13,20 +21,14 @@ class CardsSpider(scrapy.Spider):
 
     def parse(self, response):
         url = 'https://www.mtggoldfish.com'
-        for mtgset in response.css("li"):
-            if mtgset.css("img::attr(src)") and mtgset.css("a::text"):
-                link = url + mtgset.css("a::attr(href)")[0].extract()
-                yield scrapy.Request(link, self.parse_set)
-
-    def parse_set(self, response):
-        url = 'https://www.mtggoldfish.com'
         for card in response.css("tr"):
             if card.css("td.card > a::text"):
                 link = url + card.css("td.card > a::attr(href)")[0].extract()
                 cards = {
-                    'setname': card.css("td:nth-child(2)::text")[0].extract(),
+                    'setcode': card.css("td:nth-child(2)::text")[0].extract(),
                     'name': card.css("td.card > a::text")[0].extract(),
-                    'link': link,
+                    'url': link,
                     'image': card.css("td.card > a::attr(data-full-image)")[0].extract()
                 }
                 yield cards
+        print("Finished "+cards["setcode"])
